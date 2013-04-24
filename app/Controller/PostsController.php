917d4922fe44e02b610031cefc766a5e8954a46b
Anton Semenov <anton.semenov.228.1488@gmail.com>
2013-04-24 10:19:17 +0300

    first cake view

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -3,7 +3,7 @@ class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
 
     public function index() {
-        $this->set('transaction', $this->Post->find('all'));
+        $this->set('transaction', $this->Transaction->find('all'));
     }
 }   
 ?>
