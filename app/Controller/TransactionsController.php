879622476294f9a95d36827e6963d77d4dd58ad0
Anton Semenov <anton.semenov.228.1488@gmail.com>
2013-04-24 10:40:51 +0300

    first cake view

--- app/Controller/TransactionsController.php
+++ app/Controller/TransactionsController.php
@@ -3,7 +3,7 @@ class TransactionsController extends AppController {
     public $helpers = array('Html', 'Form');
 
     public function index() {
-        $this->set('transaction', $this->Transaction->find('all'));
+        $this->set('transactions', $this->Transaction->find('all'));
     }
 }   
 ?>
