7b6cff09da15fc78a164ab708836568fbfb55c2c
Anton Semenov <anton.semenov.228.1488@gmail.com>
2013-04-25 05:15:21 +0300

    Add users

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -2,11 +2,11 @@
 
 class UsersController extends AppController {
 
-    public function beforeFilter() {
+/*     public function beforeFilter() {
         parent::beforeFilter();
         $this->Auth->allow('add');
     }
-
+ */
     public function index() {
         $this->User->recursive = 0;
         $this->set('users', $this->paginate());
