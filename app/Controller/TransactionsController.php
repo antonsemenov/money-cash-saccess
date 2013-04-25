76ba710ca7e02835b23d7fc95911e979e8fa3d55
Anton Semenov <anton.semenov.228.1488@gmail.com>
2013-04-25 05:33:10 +0300

    Add users

--- app/Controller/TransactionsController.php
+++ app/Controller/TransactionsController.php
@@ -1,6 +1,7 @@
 <?php
 class TransactionsController extends AppController {
-    public $helpers = array('Html', 'Form');
+    
+	public $helpers = array('Html', 'Form');
 
     public function index() {
         $this->set('transactions', $this->Transaction->find('all'));
