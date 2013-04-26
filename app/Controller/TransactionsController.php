b0dcf8201f21bedcde31899c89eb13416ecf3475
Anton Semenov <anton.semenov.228.1488@gmail.com>
2013-04-26 16:47:50 +0300

    design stuff

--- app/Controller/TransactionsController.php
+++ app/Controller/TransactionsController.php
@@ -78,7 +78,7 @@ class TransactionsController extends AppController {
     // The owner of a post can edit and delete it
 		if (in_array($this->action, array('edit', 'delete'))) {
 			$transactionsId = $this->request->params['pass'][0];
-			if ($this->Transactions->isOwnedBy($transactionsId, $user['id'])) {
+			if ($this->Transaction->isOwnedBy($transactionsId, $user['id'])) {
 				return true;
 			}
 		}
