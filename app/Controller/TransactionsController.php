19fc83d8a48687af7ce82f105b464a932b9427cd
Anton Semenov <anton.semenov.228.1488@gmail.com>
2013-04-27 14:03:28 +0300

    user transaction delete alerts

--- app/Controller/TransactionsController.php
+++ app/Controller/TransactionsController.php
@@ -4,7 +4,7 @@ class TransactionsController extends AppController {
 	public $helpers = array('Html', 'Form');
 
     public function index() {
-		if ($this->Auth->user['role'] == 'admin')){
+		if ($this->Auth->user['role'] == 'admin'){
 			
 			$this->set('transactions', $this->Transaction->find('all'));
 			
