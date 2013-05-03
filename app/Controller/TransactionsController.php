4579887d148ef4f87c36d775621be6f787543196
Ben <ben@mysrv.local>
2013-05-03 03:57:06 +0300

    View value

--- app/Controller/TransactionsController.php
+++ app/Controller/TransactionsController.php
@@ -21,9 +21,10 @@ class TransactionsController extends AppController {
 	$sum_value = '$0.0';
 	
 	foreach ($user_transactions as $transaction){
-		$this->Session->setFlash("Begin ".gettype($transaction['Transaction']['value']));
-		$sum_value += $transaction['Transaction']['value'];
+		
+		$sum_value += (float)substr($input, strpos(, "$")+1);
 		}
+	$this->Session->setFlash("End ".$sum_value);
 	$this->set('sum_value', $sum_value);
 }
 	
